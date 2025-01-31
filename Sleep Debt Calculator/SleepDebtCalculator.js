47db19bb636c8e95f1836ea7a5a9749c12b2a71c
Giorgos Basiaris <georgebsr@outlook.com>
2025-01-31 13:20:06 +0200

    Update SleepDebtCalculator.js

--- Sleep Debt Calculator/SleepDebtCalculator.js
+++ Sleep Debt Calculator/SleepDebtCalculator.js
@@ -1,1 +1,54 @@
+function getSleepHours(day) {
+  switch(day) {
+    case 'monday':
+      return 8;
+    case 'tuesday':
+      return 8;
+    case 'wednesday':
+      return 7;
+    case 'thursday':
+      return 8;
+    case 'friday':
+      return 6;
+    case 'saturday':
+      return 8;
+    case 'sunday':
+      return 8;
+    default:
+      return 0;
+  }
+}
 
+//console.log(getSleepHours('friday')); //test
+
+const getActualSleepHours = () => 
+  getSleepHours('monday') +
+  getSleepHours('tuesday') +
+  getSleepHours('wednesday') +
+  getSleepHours('thursday') +
+  getSleepHours('friday') +
+  getSleepHours('saturday') +
+  getSleepHours('sunday');
+
+const getIdealSleepHours = () => {
+  const idealHours = 8;
+  return idealHours * 7;
+};
+
+//console.log(getActualSleepHours('friday')); //Test
+//console.log(getIdealSleepHours()); //Test
+
+const calculateSleepDebt = () => {
+  const actualSleepHours = getActualSleepHours();
+  const idealSleepHours = getIdealSleepHours();
+
+  if (actualSleepHours === idealSleepHours) {
+    console.log('Perfect amount of sleep');
+  } else if (actualSleepHours > idealSleepHours) {
+    console.log('More sleep than needed acquired. You got ' + (actualSleepHours - idealSleepHours) + ' hour(s) more.');
+  } else {
+    console.log('You should get more sleep. You got ' + (idealSleepHours - actualSleepHours) + ' hour(s) less.');
+  }
+}
+
+calculateSleepDebt();
